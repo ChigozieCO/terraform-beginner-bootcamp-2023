9c4aa2964cb617aaca61f3eb3329d6c4182aec98
ChigozieCO <107365067+ChigozieCO@users.noreply.github.com>
2023-10-07 14:32:00 +0100

    #38 Update postCreateCommand.sh

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -3,8 +3,8 @@
 # Install Terraform CLI, Generate tfrc credentials & set tf alias for codespace
 
 source ./bin/install_terraform_cli_codespace
-source ./bin/generate_tfrc_credentials_codespace
-source ./bin/set_tf_alias_codespace
+source ./bin/generate_tfrc_credentials
+source ./bin/set_tf_alias
 cp $PROJECT_ROOT/terraform.tfvars.example $PROJECT_ROOT/terraform.tfvars
 
 
