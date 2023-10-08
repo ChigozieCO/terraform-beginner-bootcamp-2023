9d5447387e4070ba7836d1ecd719367ee162bebf
ChigozieCO <107365067+ChigozieCO@users.noreply.github.com>
2023-10-08 03:00:01 +0100

    38 implement use of codespaces (#39)
    * #38 Create a devcontainer.json file
    
    * #38 Create postCreateCommand.sh
    
    * #38 Update postCreateCommand.sh
    
    * #38 Create install_terraform_cli_codespace
    
    * #38 Update devcontainer.json
    
    * #38 Update devcontainer.json again
    
    * #38 Update devcontainer.json
    
    * #38 Update devcontainer.json yet again
    
    * #38 Update devcontainer.json; hope it works now
    
    * #38 Update postCreateCommand.sh, add aws cli
    
    * #38 Create install_aws_cli_codespace
    
    * #38 Make the necessary scripts executable
    
    * #38 Added a new script and modified postCreateCommand
    
    * :#38 Added an extension

---
+++ .devcontainer/postCreateCommand.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+# Install Terraform CLI, Generate tfrc credentials & set tf alias for codespace
+source ./bin/install_terraform_cli_codespace
+source ./bin/generate_tfrc_credentials
+source ./bin/set_tf_alias_codespace
+cp $PROJECT_ROOT/terraform.tfvars.example $PROJECT_ROOT/terraform.tfvars
+
+# Install AWS ClI
+source ./bin/install_aws_cli_codespace
+source ./bin/set_tf_alias_codespace
+
+
+      
+      
+      
+      
